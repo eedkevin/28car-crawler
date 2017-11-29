d08b43c9cce90cd29a79a62092971c0ccf0b268f
eedkevin <eedkevin@gmail.com>
2017-11-29 18:00:28 +0800

    add comment crawling

--- database/car.go
+++ database/car.go
@@ -1,19 +1,25 @@
 package database
 
 type Car struct {
-	Vid string
-	Sid string
-	Type string
-	Brand string
-	Model string
-	Seat string
-	Engine string
-	Shift string
+	Vid            string
+	Sid            string
+	Type           string
+	Brand          string
+	Model          string
+	Seat           string
+	Engine         string
+	Shift          string
 	ProductionYear string
-	Description string
-	OrigPrice int
-	CurrPrice int
-	Contact string
-	UploadTime string
-	Hash string
+	Description    string
+	OrigPrice      int
+	CurrPrice      int
+	Contact        string
+	Comments       []Comment
+	UploadTime     string
+	Hash           string
+}
+
+type Comment struct {
+	Replier string
+	Msg     string
 }
